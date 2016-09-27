2699367a4a6180ea67e9c0ef4c9351e067b3920c
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2016-09-27 15:05:44 -0400

    Initial Commit

---
+++ FlexNetworking/FlexNetworking.h
@@ -0,0 +1,19 @@
+//
+//  FlexNetworking.h
+//  FlexNetworking
+//
+//  Created by Dennis Lysenko on 27-09-16.
+//  Copyright © 2016 Dennis Lysenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlexNetworking.
+FOUNDATION_EXPORT double FlexNetworkingVersionNumber;
+
+//! Project version string for FlexNetworking.
+FOUNDATION_EXPORT const unsigned char FlexNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexNetworking/PublicHeader.h>
+
+
